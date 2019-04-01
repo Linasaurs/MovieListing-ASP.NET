5b18aa34cccc8d1f8bf9f22677b09b28a4c659d4
Lina ElTohamy <Lina.ElTohamy@itworx.com>
2019-04-01 20:14:03 +0200

    first commit

---
+++ MoviesAss1/Controllers/MovieController.cs
@@ -0,0 +1,99 @@
+﻿using MoviesAss1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoviesAss1.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View(Movie.MovieList);
+        }
+
+        // GET: Movie/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Movie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movie/Create
+        [HttpPost]
+        public ActionResult Create(Movie newMovie)
+        {
+            try
+            {
+                Movie.AddMovie(newMovie);
+                Index();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Edit/5
+        public ActionResult Edit(int Id)
+        {
+            var mov = Movie.MovieList.Where(s => s.id == Id).FirstOrDefault();
+            return View(mov);
+        }
+
+        // POST: Movie/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int Id, Movie editMovie)
+        {
+            try
+            {
+                var mov = Movie.MovieList.Where(s => s.id == Id).FirstOrDefault();
+                mov.name = editMovie.name;
+                mov.mainActor = editMovie.mainActor;
+                mov.releaseDate = editMovie.releaseDate;
+                mov.website = editMovie.website;
+                mov.countryId = editMovie.countryId;
+                Index();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Delete/5
+        public ActionResult Delete(int Id)
+        {
+            var mov = Movie.MovieList.Where(s => s.id == Id).FirstOrDefault();
+            return View(mov);
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int Id, Movie delMovie)
+        {
+            try
+            {
+                var mov = Movie.MovieList.Where(s => s.id == Id).FirstOrDefault();
+                Movie.MovieList.Remove(mov);
+                mov = null;
+                Index();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MoviesAss1/Models/Movie.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MoviesAss1.Models
+{
+    public enum Country
+    {
+        USA,
+        England,
+        Egypt,
+        France,
+        Italy
+    }
+
+    public class Movie
+    {
+        public int id { set; get; }
+        [Required]
+        [Display(Name = "Movie Title")]
+        public string name { set; get; }
+        [Display(Name = "Main Actor")]
+        public string mainActor { set; get; }
+        [Required]
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime releaseDate { set; get; }
+        [Display(Name = "Website")]
+        public string website { set; get; }
+        [Display(Name = "Country")]
+        public Country countryId { set; get; }
+
+        //Constructors
+        Movie(int _id, string _name, string _mainActor, DateTime _releaseDate, string _website, Country _countryId)
+        {
+            id = _id;
+            name = _name;
+            mainActor = _mainActor;
+            releaseDate = _releaseDate;
+            website = _website;
+            countryId = _countryId;
+        }
+
+        public Movie()
+        {
+            id = idCounter++;
+        }
+
+
+        //Add movie
+        public static void AddMovie (Movie newMovie)
+        {
+            MovieList.Add(newMovie);
+        }
+
+        // Test Data
+        static Movie Sharknado = new Movie(0, "Sharknado", "Tara Reid", new DateTime(2013, 11, 7), "https://www.rottentomatoes.com/m/sharknado_2013", 0);
+        static Movie Sharknado2 = new Movie(0, "Sharknado 2: The Second One", "Tara Reid", new DateTime(2014, 10, 7), "https://www.rottentomatoes.com/m/sharknado_2_the_second_one", 0);
+        public static List<Movie> MovieList = new List<Movie>() { Sharknado, Sharknado2 };
+        static public int idCounter = 2;
+
+
+
+    }
+
+}
